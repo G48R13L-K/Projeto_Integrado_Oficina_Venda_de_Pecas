851216de85cff8fbf6f42d928178f9abcc3c89c2
GABRIELAUGUSTOKAPPENBERG <kappenberg4@gmail.com>
2025-08-05 21:52:15 -0300

    Criação das Classes para o banco de dados

---
+++ Projeto Integrado/Projeto Integrado/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projeto_Integrado
+{
+    public class Cliente
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int CPF { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+
+    }
+}
---
+++ Projeto Integrado/Projeto Integrado/Peca.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projeto_Integrado
+{
+    public class Peca
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+
+    }
+}

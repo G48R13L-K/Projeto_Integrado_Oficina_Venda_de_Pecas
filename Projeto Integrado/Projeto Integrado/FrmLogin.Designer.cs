346e9e56fbf282358c2429c284549deb9c9930db
Miguel <poleom2@gmail.cpm>
2025-08-05 21:44:16 -0300

    Criação da tela Login e Vendas

---
+++ Projeto Integrado/Projeto Integrado/FrmLogin.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Projeto_Integrado
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            txtUsuario = new TextBox();
+            txtSenha = new TextBox();
+            btnEntrar = new Button();
+            btnFechar = new Button();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Navy;
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(4, 4, 4, 4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(390, 529);
+            panel1.TabIndex = 0;
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Font = new Font("Arial Narrow", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtUsuario.Location = new Point(529, 120);
+            txtUsuario.Margin = new Padding(4, 4, 4, 4);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(367, 44);
+            txtUsuario.TabIndex = 1;
+            txtUsuario.Text = "USUARIO";
+            // 
+            // txtSenha
+            // 
+            txtSenha.Font = new Font("Arial Narrow", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtSenha.Location = new Point(529, 243);
+            txtSenha.Margin = new Padding(4, 4, 4, 4);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(367, 44);
+            txtSenha.TabIndex = 2;
+            txtSenha.Text = "SENHA";
+            // 
+            // btnEntrar
+            // 
+            btnEntrar.BackColor = Color.White;
+            btnEntrar.FlatAppearance.BorderSize = 0;
+            btnEntrar.FlatStyle = FlatStyle.Flat;
+            btnEntrar.Font = new Font("Arial Narrow", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnEntrar.Location = new Point(638, 363);
+            btnEntrar.Margin = new Padding(4, 4, 4, 4);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.Size = new Size(150, 57);
+            btnEntrar.TabIndex = 3;
+            btnEntrar.Text = "ENTRAR";
+            btnEntrar.UseVisualStyleBackColor = false;
+            // 
+            // btnFechar
+            // 
+            btnFechar.FlatAppearance.BorderSize = 0;
+            btnFechar.FlatStyle = FlatStyle.Flat;
+            btnFechar.Location = new Point(918, 0);
+            btnFechar.Name = "btnFechar";
+            btnFechar.Size = new Size(56, 34);
+            btnFechar.TabIndex = 4;
+            btnFechar.Text = "X";
+            btnFechar.UseVisualStyleBackColor = true;
+            // 
+            // FrmLogin
+            // 
+            AutoScaleDimensions = new SizeF(13F, 33F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(192, 192, 255);
+            ClientSize = new Size(975, 529);
+            Controls.Add(btnFechar);
+            Controls.Add(btnEntrar);
+            Controls.Add(txtSenha);
+            Controls.Add(txtUsuario);
+            Controls.Add(panel1);
+            Font = new Font("Arial Narrow", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "FrmLogin";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TextBox txtUsuario;
+        private TextBox txtSenha;
+        private Button btnEntrar;
+        private Button btnFechar;
+    }
+}

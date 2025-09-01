4f9bcb795bf69560d481c9282cd2c56530fb1dca
Miguel <poleom2@gmail.cpm>
2025-09-01 20:24:30 -0300

    Mudei a cor das letras do relatorio vendas

--- Projeto Integrado/Projeto Integrado/FrmRelatorio.Designer.cs
+++ Projeto Integrado/Projeto Integrado/FrmRelatorio.Designer.cs
@@ -49,19 +49,19 @@
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Segoe UI", 12F);
             label1.ForeColor = SystemColors.AppWorkspace;
-            label1.Location = new Point(20, 17);
+            label1.Location = new Point(25, 21);
             label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(86, 28);
+            label1.Size = new Size(105, 32);
             label1.TabIndex = 0;
             label1.Text = "Pesquisa";
             // 
             // txtPesquisa
             // 
-            txtPesquisa.Location = new Point(114, 18);
+            txtPesquisa.Location = new Point(142, 22);
             txtPesquisa.Margin = new Padding(2);
             txtPesquisa.Name = "txtPesquisa";
-            txtPesquisa.Size = new Size(677, 27);
+            txtPesquisa.Size = new Size(845, 31);
             txtPesquisa.TabIndex = 1;
             txtPesquisa.TextChanged += txtPesquisa_TextChanged;
             // 
@@ -74,11 +74,11 @@
             groupBox1.Controls.Add(dataGridView1);
             groupBox1.Font = new Font("Segoe UI", 12F);
             groupBox1.ForeColor = SystemColors.AppWorkspace;
-            groupBox1.Location = new Point(9, 47);
+            groupBox1.Location = new Point(11, 59);
             groupBox1.Margin = new Padding(2);
             groupBox1.Name = "groupBox1";
             groupBox1.Padding = new Padding(2);
-            groupBox1.Size = new Size(882, 445);
+            groupBox1.Size = new Size(1102, 556);
             groupBox1.TabIndex = 2;
             groupBox1.TabStop = false;
             groupBox1.Text = "Relatório de Vendas";
@@ -90,10 +90,10 @@
             btnCancelar.FlatAppearance.BorderSize = 0;
             btnCancelar.FlatStyle = FlatStyle.Flat;
             btnCancelar.ForeColor = Color.Black;
-            btnCancelar.Location = new Point(718, 387);
+            btnCancelar.Location = new Point(898, 484);
             btnCancelar.Margin = new Padding(2);
             btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(146, 44);
+            btnCancelar.Size = new Size(182, 55);
             btnCancelar.TabIndex = 1;
             btnCancelar.Text = "Excluir";
             btnCancelar.UseVisualStyleBackColor = false;
@@ -105,10 +105,10 @@
             btnMaisVendas.FlatAppearance.BorderSize = 0;
             btnMaisVendas.FlatStyle = FlatStyle.Flat;
             btnMaisVendas.ForeColor = Color.Black;
-            btnMaisVendas.Location = new Point(11, 387);
+            btnMaisVendas.Location = new Point(14, 484);
             btnMaisVendas.Margin = new Padding(2);
             btnMaisVendas.Name = "btnMaisVendas";
-            btnMaisVendas.Size = new Size(182, 44);
+            btnMaisVendas.Size = new Size(228, 55);
             btnMaisVendas.TabIndex = 1;
             btnMaisVendas.Text = "Nova Venda";
             btnMaisVendas.UseVisualStyleBackColor = false;
@@ -121,10 +121,10 @@
             btnEditar.FlatAppearance.BorderSize = 0;
             btnEditar.FlatStyle = FlatStyle.Flat;
             btnEditar.ForeColor = Color.Black;
-            btnEditar.Location = new Point(555, 387);
+            btnEditar.Location = new Point(694, 484);
             btnEditar.Margin = new Padding(2);
             btnEditar.Name = "btnEditar";
-            btnEditar.Size = new Size(146, 44);
+            btnEditar.Size = new Size(182, 55);
             btnEditar.TabIndex = 1;
             btnEditar.Text = "Editar";
             btnEditar.UseVisualStyleBackColor = false;
@@ -148,11 +148,11 @@
             dataGridViewCellStyle2.SelectionForeColor = SystemColors.ActiveCaptionText;
             dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
             dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
-            dataGridView1.Location = new Point(4, 25);
+            dataGridView1.Location = new Point(5, 31);
             dataGridView1.Margin = new Padding(2);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(874, 352);
+            dataGridView1.Size = new Size(1092, 440);
             dataGridView1.TabIndex = 0;
             dataGridView1.CellClick += dataGridView1_CellClick;
             dataGridView1.CellContentClick += dataGridView1_CellContentClick;
@@ -165,10 +165,10 @@
             btnFechar.FlatStyle = FlatStyle.Flat;
             btnFechar.Font = new Font("Segoe UI", 12F);
             btnFechar.ForeColor = SystemColors.AppWorkspace;
-            btnFechar.Location = new Point(847, 1);
+            btnFechar.Location = new Point(1059, 1);
             btnFechar.Margin = new Padding(2);
             btnFechar.Name = "btnFechar";
-            btnFechar.Size = new Size(44, 55);
+            btnFechar.Size = new Size(55, 69);
             btnFechar.TabIndex = 1;
             btnFechar.Text = "X";
             btnFechar.UseVisualStyleBackColor = false;
@@ -176,12 +176,12 @@
             // 
             // FrmRelatorio
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(192, 192, 255);
             BackgroundImage = Properties.Resources.WhatsApp_Image_2025_08_18_at_21_17_26;
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(902, 503);
+            ClientSize = new Size(1128, 629);
             Controls.Add(label1);
             Controls.Add(groupBox1);
             Controls.Add(btnFechar);
